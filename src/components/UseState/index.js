cd0ed16319801ecb51086df5d96ed85f5b4ff90b
Barca <143180305+barcaBC@users.noreply.github.com>
2024-01-26 10:14:22 +0000

    bubu

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,16 +1,25 @@
-import './UseState.css';
+import "./UseState.css";
+import { useState } from "react";
 
 const UseState = () => {
+  // console.log(useState("light"));
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="state">
+    <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
-      <button>Dark</button>
-      <button>Light</button>
-      <h2>DISPLAY COUNT HERE</h2>
-      <button>
+      {/* <button onClick={() => setTheme("dark")}>Dark</button> */}
+      <button onClick={toggleTheme}>Change Theme</button>
+      <h2>{count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
         Increment
       </button>
-      <button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
         Decrement
       </button>
     </div>
